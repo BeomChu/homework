16c93c1dd4f35e5e4a5cd57495ecabd13aaecabf
Beomchu <izekfzha@gmail.com>
2023-04-03 19:36:23 +0900

    create project

---
+++ src/main/java/sharetreat/SharetreatApplication.java
@@ -0,0 +1,13 @@
+package sharetreat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharetreatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharetreatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sharetreat/SharetreatApplicationTests.java
@@ -0,0 +1,13 @@
+package sharetreat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharetreatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
